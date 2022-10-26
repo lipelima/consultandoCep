1abd30f844fe8b59446b454fa17ff9226d4297ee
Felipe Lima <62923236+LipeLima@users.noreply.github.com>
2022-10-26 19:17:45 -0300

    update 26/10

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ function zerarCampo() {
     document.querySelector('#cep').value=("");
 }
 
+function escondeCampo(){
+    document.querySelector('#cep').style.display= "none";
+    document.querySelector('#btn').style.display= "none";
+    document.querySelector('#titulo').style.display= "none";
+}
+
+function btnRetorno(){
+    location.reload();
+}
+
+
 function consultAddress() {
 
     let valorCep = document.querySelector('#cep').value;
@@ -22,16 +33,20 @@ function consultAddress() {
 
 
 function showAddress(dados) {
+
     let resultado = document.querySelector('#resultado');
 
     if (dados.erro) {
-        resultado.innerHTML = "Não foi possivel localizar endereço";
+        resultado.innerHTML = "Não foi possivel localizar o endereço";
     } else {
-        resultado.innerHTML = `<p>Endereço: ${dados.logradouro}</p>
-                                <p>Bairro: ${dados.bairro}</p>
-                                <p>Localidade: ${dados.localidade} - ${dados.uf}</p>`
-                                
-    }
+        escondeCampo();   
+        resultado.innerHTML = ` <p class= "subtitulo">Cep:</p> ${dados.cep}
+                                <p class= "subtitulo">Endereço:</p> ${dados.logradouro}
+                                <p class= "subtitulo">Bairro:</p> ${dados.bairro}</p>
+                                <p class= "subtitulo">Localidade:</p> ${dados.localidade} - ${dados.uf}
+                                <p class= "subtitulo">DDD:</p> ${dados.ddd}</p> 
+                                <button onclick="btnRetorno()" id="btnVoltar"> Voltar </button>`                           
+    }   
 }
 
 
